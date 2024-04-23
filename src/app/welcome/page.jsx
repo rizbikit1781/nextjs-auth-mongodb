b738ea52479c848c9f2c8ae4c20e449738764a1d
donnysait88 <119543204+donnysait88@users.noreply.github.com>
2024-04-23 00:03:12 -0600

    Test

--- src/app/welcome/page.jsx
+++ src/app/welcome/page.jsx
@@ -14,6 +14,7 @@ const Welcome = () => {
 
   return (
     <div >
+
         <Navbar session={session} />
         <div className='m-5'>
             <h3 className='text-3xl my-3 font-bold'>Welcome {session?.user?.name}</h3>
@@ -21,6 +22,7 @@ const Welcome = () => {
             <hr className='py-2 '/>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas unde laboriosam quibusdam! Laborum perferendis ipsum est facere accusantium nostrum maxime earum praesentium alias rem ullam, at placeat deserunt in similique!</p>
         </div>
+        
     </div>
   )
 }
